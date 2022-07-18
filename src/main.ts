ba0fedf9a3012b958c434e698098cc29d1be2d53
miradii <mrtuz@protonmail.com>
2022-07-18 19:05:03 +0430

    added prisma and created models

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  // Configure Swagger
+  const config = new DocumentBuilder()
+    .setTitle('Products API')
+    .setDescription('A simple Rest API in Nest')
+    .setVersion('1.0')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
+  await app.listen(8080);
 }
 bootstrap();
